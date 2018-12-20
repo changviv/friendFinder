1af76f206b1ba44b4e51b4b07908239886f8b3dc
Vivien <vchang103@gmail.com>
2018-12-19 22:31:56 -0800

    turn number into strings, and fix survey html

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friendsArray = [
   {
     "name":"Vivien",
     "photo":"https://cdn.vox-cdn.com/thumbor/v8dbQ0w86YaX4UGRQ72xYnwt95M=/0x0:1200x675/1200x800/filters:focal(504x242:696x434)/cdn.vox-cdn.com/uploads/chorus_image/image/60487971/1200px_Jigglypuff_anime.0.png",
-    "scores":[5,1,4,4,5,1,2,5,4,1]
+    "scores":["5","1","4","4","5","1","2","5","4","1"]
   },
   {
     "name":"Stewart",
     "photo":"https://thumbs.mic.com/YjNmZjg5YjcwYSMvSnZrM0FUM010MGo1ejBPS1M0dFduQTUtVWpBPS8xNngyMjM6MTM0N3g4NTcvODAweDQ1MC9maWx0ZXJzOmZvcm1hdChqcGVnKTpxdWFsaXR5KDgwKS9odHRwczovL3MzLmFtYXpvbmF3cy5jb20vcG9saWN5bWljLWltYWdlcy9va2JoYXJ5eTVlMHQwenczZXNuNHd5MWdqdzgwcm1ob3lmZmlnaXluMHQxbmN3ZHJtYm9sZmRsempkNndoeWJiLmpwZw.jpg",
-    "scores":[5,1,4,3,2,4,5,1,3,2]
+    "scores":["5","1","4","3","2","4","5","1","3","2"]
   },
   {
     "name":"Kevin",
     "photo":"https://akns-images.eonline.com/eol_images/Entire_Site/2015725/rs_634x920-150825112125-634-mccauley-culkin-home-alone-2-08255.jpg?fit=inside|900:auto&output-quality=90",
-    "scores":[1,2,3,4,2,3,5,4,1,2]
+    "scores":["1","2","3","4","2","3","5","4","1","2"]
   },
   {
     "name":"Ichigo",
     "photo":"https://vignette.wikia.nocookie.net/bleach/images/5/52/591Ichigo_profile.png/revision/latest?cb=20170905012136&path-prefix=en",
-    "scores":[5,5,5,4,2,4,2,3,4,5]
+    "scores":["5","5","5","4","2","4","2","3","4","5"]
   },
 
 ]
